bcc4eb9427403accaeed3f88b21381b9bd0e2704
Brandon Bennett <bennetb@gmail.com>
2015-01-20 20:44:35 -0700

    Cleanup tests

--- tvdb_test.go
+++ tvdb_test.go
@@ -7,72 +7,76 @@ import (
 )
 
 const (
-	apiKey     = "DECE3B6B5464C552"
-	testUser   = "34A8615ABE815874"
-	simpsonsID = 71663
-	futuramaID = 73871
+	apiKey       = "DECE3B6B5464C552"
+	testUser     = "34A8615ABE815874"
+	simpsonsID   = 71663
+	simpsonsIMDB = "tt0096697"
+	simpsonsName = "The Simpsons"
+	futuramaID   = 73871
 )
 
 var tvdb *TVDB
 
 // TestGetSeries tests the GetSeries function.
 func TestGetSeries(t *testing.T) {
-	seriesList, err := tvdb.GetSeries("The Simpsons")
+	t.Logf("Finding series with name '%s'", simpsonsName)
+	seriesList, err := tvdb.GetSeries(simpsonsName)
 
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	for _, series := range seriesList {
-		if series.SeriesName == "The Simpsons" {
+		if series.SeriesName == simpsonsName {
 			return
 		}
 	}
 
-	t.Error("No 'The Simpsons' title could be found.")
+	t.Errorf("Expected to find series '%s' got '%s'", simpsonsName, seriesList)
 }
 
 // TestGetSeriesByID tests the GetSeriesByID function.
 func TestGetSeriesByID(t *testing.T) {
+	t.Logf("Getting series with id '%d'", simpsonsID)
 	series, err := tvdb.GetSeriesByID(simpsonsID)
-
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
-	if series.SeriesName != "The Simpsons" {
-		t.Error("ID lookup for 'simpsonsID' failed.")
+	if series.SeriesName != simpsonsName {
+		t.Errorf("Lookup for ID '%d'. Expected name of '%s' got '%s'",
+			simpsonsID, simpsonsName, series.SeriesName)
 	}
 }
 
 // TestGetSeriesByRemoteID tests the GetSeriesByRemoteID function.
 func TestGetSeriesByRemoteID(t *testing.T) {
-	series, err := tvdb.GetSeriesByRemoteID(IMDB, "tt0096697")
-
+	t.Logf("Getting series with IMDB ID '%s'", simpsonsIMDB)
+	series, err := tvdb.GetSeriesByRemoteID(IMDB, simpsonsIMDB)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
-	if series.SeriesName != "The Simpsons" {
-		t.Error("IMDb ID lookup for 'tt0096697' failed.")
+	if series.SeriesName != simpsonsName {
+		t.Errorf("Expectted series name of '%s' got '%s' for IMDB ID of '%s' failed.")
 	}
 }
 
 // TestSearchSeries tests the SearchSeries function.
 func TestSearchSeries(t *testing.T) {
-	seriesList, err := tvdb.SearchSeries("The Simpsons", 5)
-
+	t.Logf("Searching for series with name '%s'", simpsonsName)
+	seriesList, err := tvdb.SearchSeries(simpsonsName, 5)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	for _, series := range seriesList {
-		if series.SeriesName == "The Simpsons" {
+		if series.SeriesName == simpsonsName {
 			return
 		}
 	}
 
-	t.Error("No 'The Simpsons' title could be found.")
+	t.Errorf("Expected to find series '%s' got '%s'", simpsonsName, seriesList)
 }
 
 func seriesIDExists(favs []int, seriesID int) bool {
@@ -86,9 +90,10 @@ func seriesIDExists(favs []int, seriesID int) bool {
 
 // TestSeriesGetDetail tests the Series type's GetDetail function.
 func TestGetSeriesFull(t *testing.T) {
+	t.Logf("Getting Full series for seriesID '%d'", simpsonsID)
 	series, err := tvdb.GetSeriesFull(simpsonsID)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if series.Seasons == nil {
@@ -97,10 +102,11 @@ func TestGetSeriesFull(t *testing.T) {
 }
 
 func TestUserFav(t *testing.T) {
+	t.Logf("Querying favorites for userID '%s'", testUser)
 	// Test user with one favorite
-	favs, err := tvdb.UserFav("34A8615ABE815874")
+	favs, err := tvdb.UserFav(testUser)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if !seriesIDExists(favs, simpsonsID) {
